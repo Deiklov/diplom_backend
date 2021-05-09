047f869dd99363cb1deec33bfca2e12bcd7b842d
Deiklov <romanov408g@mail.ru>
2021-05-09 23:06:26 +0300

    without verbose mode

--- internal/services/prediction/gun_run.sh
+++ internal/services/prediction/gun_run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 lsof -ti tcp:5000 | xargs kill
-export CUDA_VISIBLE_DEVICES=3
-gunicorn -w 8 -b 0.0.0.0:5000 main:app
+gunicorn --env CUDA_VISIBLE_DEVICES=3 -w 8 -b 0.0.0.0:5000 main:app &
